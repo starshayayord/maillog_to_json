f3bb7da85c69328ec416d4b6a84dff6660ec8177
linikova <linikova@skbkontur.ru>
2016-04-13 18:44:12 +0500

    fix list scope

--- maillog_to_json.py
+++ maillog_to_json.py
@@ -1,24 +1,36 @@
 import re
-import pickle
 from sys import argv
-from collections import defaultdict
-from json import dumps, dump, loads, JSONEncoder, JSONDecoder
+from json import dumps, dump
+import copy
+'''filename, out_filename  = argv'''
+#filename = "D:\GitProjects\maillog_to_json\maillog.0"
+filename = 'C:\Users\linikova\Desktop\maillog.txt'
+out_filename = 'C:\Users\linikova\Desktop\maillog_parsed.txt'
 
-'''filename = argv'''
-filename = "C:\Users\linikova\Desktop\maillog.txt"
 
 
 class Message(object):
     id = ""
     client = ""
     sender = ""
-    recipient = set()
 
     def __init__(self, id):
         self.id = id
+        self.recipient = list()
+
+    def addRecipient(self, rcpt):
+        self.recipient.append(rcpt)
+
+    def get_rejected(self):
+        m = REProper(self.sender)
+        if m.match(r"(.*?@(?!kontur.ru|buhonline.ru|kontur-extern.ru|e-kontur.ru).*$)"):
+            return True
+        else:
+            return False
 
 
 class REProper(object):
+
     def __init__(self, matchstring):
         self.matchstring = matchstring
 
@@ -27,13 +39,14 @@ class REProper(object):
         return bool(self.rematch)
 
     def group(self, i):
-        return self.rematch.group(i)
+        return self.rematch.group()
 
 
 def set_default(obj):
     if isinstance(obj, set):
         return list(obj)
     raise TypeError
+
 
 def notlast(itr):
     itr = iter(itr)  # ensure we have an iterator
@@ -42,58 +55,73 @@ def notlast(itr):
         yield prev
         prev = item
 
+
 def lineparse(line):
     elem = line.split(' ', 6)
 
     if (bool(re.match(r"([a-zA-Z0-9:]{1,})", elem[5]))):
         e_id = elem[5].strip(':')
         m = REProper(line)
-        email = Message(e_id)
-        if m.match(r".*client=(.*).*"):
+        email = copy.copy(Message(e_id))
+        if m.match(r": client=(.*).*"):
             email.client = m.group(1)
             return email
 
-        elif m.match(r"from=<(.*?)>"):
+        elif m.match(r": from=<(.*?)>"):
             email.sender = m.group(1)
             return email
 
-        elif m.match(r"to=<(.*?)>"):
-            email.recipient.add(m.group(1))
+        elif m.match(r": to=<(.*?)>"):
+            email.addRecipient(m.group(1))
             return email
 
     return None
 
 
-emailarray = defaultdict(Message)
-with open(filename, mode='r') as f:
-    for line in f:
-        message = lineparse(line)
-        if bool(message):
-            if message.id in emailarray:
-                current = emailarray[message.id]
-                if bool(message.client):
-                    current.client = message.client
-                elif bool(message.sender):
-                    current.sender = message.sender
-                elif bool(message.recipient):
-                    current.recipient.union(message.recipient)
-            else:
-                emailarray[message.id] = message
-out_filename = 'C:\maillog_parsed.txt'
-with open(out_filename, 'w') as outfile:
-    outfile.write("[\n")
-for mail in notlast(emailarray.values()):
-    # mail.recipient = dumps(mail.recipient, default=set_default)
+def result_to_file(out_filename, rejectedarray):
+    with open(out_filename, 'w') as outfile:
+        outfile.write("[\n")
+    for mail in notlast(rejectedarray.values()):
+        # mail.recipient = dumps(mail.recipient, default=set_default)
+        mail.recipient = dumps(mail.recipient, default=set_default)
+        mail = dumps(mail, default=lambda o: o.__dict__)
+        with open(out_filename, 'a') as outfile:
+            dump(mail, outfile)
+            outfile.write(",\n")
+    # process last element with no delimiter
+    mail = rejectedarray.values()[-1]
     mail.recipient = dumps(mail.recipient, default=set_default)
     mail = dumps(mail, default=lambda o: o.__dict__)
     with open(out_filename, 'a') as outfile:
         dump(mail, outfile)
-        outfile.write(",\n")
-#process last element with no delimiter
-mail = emailarray.values()[-1]
-mail.recipient = dumps(mail.recipient, default=set_default)
-mail = dumps(mail, default=lambda o: o.__dict__)
-with open(out_filename, 'a') as outfile:
-    dump(mail, outfile)
-with open(out_filename, 'a') as outfile:
-    outfile.write("\n]")
+    with open(out_filename, 'a') as outfile:
+        outfile.write("\n]")
+
+
+
+
+
+emailarray = dict()
+with open(filename, mode='r') as f:
+    for line in f:
+        msg = lineparse(line)
+        if bool(msg):
+            if msg.id in emailarray:
+                if bool(msg.client):
+                    emailarray[msg.id].client = msg.client
+                elif bool(msg.sender):
+                    emailarray[msg.id].sender = msg.sender
+                elif bool(msg.recipient):
+                    if not msg.recipient in emailarray[msg.id].recipient:
+                        emailarray[msg.id].addRecipient(msg.recipient)
+            else:
+                emailarray[msg.id] = msg
+# parse rejected
+rejectedarray = dict()
+for message_id in emailarray.keys():
+    if (emailarray[message_id]).get_rejected():
+        rejectedarray[message_id] = emailarray[message_id]
+
+
+# results to file
+#result_to_file(out_filename, rejectedarray)
